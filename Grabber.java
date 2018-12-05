d86dea4e165280bde711b8cd3f1afc106b647a2b
Rohan <40905346+Rohan1215@users.noreply.github.com>
2018-12-04 22:31:52 -0800

    Final Grabber
    dpad lander
    Y and A Grabber Winch
    Triggers are Spinning Grabber mechanism
    Bumper is for tilting

--- Grabber.java
+++ Grabber.java
@@ -6,43 +6,60 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="TestGrabber", group ="Testing")
 public class Grabber extends LinearOpMode{
-    private DcMotor spinner;
+    private DcMotor grabber;
+    private DcMotor lander;
     private DcMotor winch;
-    private Servo tiltl;
-    private Servo tiltr;
+    private Servo tilt1;
+    private Servo tilt2;
     @Override
     public void runOpMode(){
-        spinner = hardwareMap.get(DcMotor.class, "Spinner");
+        grabber = hardwareMap.get(DcMotor.class, "Grabber");
         winch = hardwareMap.get(DcMotor.class, "Winch");
-        tiltl = hardwareMap.get(Servo.class, "Tiltl");
-        tiltr = hardwareMap.get(Servo.class, "Tiltr");
+        lander=hardwareMap.get(DcMotor.class,"Lander");
+        tilt1 = hardwareMap.get(Servo.class, "Tilt1");
+        tilt2 = hardwareMap.get(Servo.class, "Tilt2");
         waitForStart();
         double SpinPower=0;
-        double WnchPower=0;
-        double Rbrmultiplier=0.5;
-        double Wnchmultiplier=0.5;
-        tiltl.setPosition(0);
-        tiltr.setPosition(0);
-        while (opModeIsActive()){
-            if(this.gamepad2.right_stick_y!=0) {
-                SpinPower = Rbrmultiplier * this.gamepad2.right_stick_y;
-                spinner.setPower(SpinPower);
+        double LanderPower=0;
+        double Grbrmultiplier=0.5;
+        double WinchMultiplier=0.5;
+        tilt1.setPosition(0);
+        tilt2.setPosition(0);
+        while (opModeIsActive()) {
+            if (this.gamepad2.left_trigger > 0) {
+                SpinPower = -Grbrmultiplier * this.gamepad2.left_trigger;
+                grabber.setPower(SpinPower);
             }
 
-            if (this.gamepad2.left_stick_y!=0){
-                WnchPower=this.gamepad2.left_stick_y*Wnchmultiplier;
-                winch.setPower(WnchPower);
+            if (this.gamepad2.right_trigger > 0) {
+                SpinPower = this.gamepad2.right_trigger * Grbrmultiplier;
+                grabber.setPower(SpinPower);
             }
+          /*  if (this.gamepad2.left_stick_y!=0){
+                LanderPower=this.gamepad2.left_stick_y*TiltMultiplier;
+                .setPower(LanderPower);
+            } ITS A SERVO IT DOESN'T HAVE POWERRRRRRRRRRRR*/
+            if (gamepad2.dpad_up) {
+                winch.setPower(WinchMultiplier);
+            }
+            if (gamepad2.dpad_down) {
+                winch.setPower(-WinchMultiplier);
 
-        if(gamepad2.dpad_up){
-            tiltl.setPosition(0.1);
-            tiltr.setPosition(-0.1);
-        }
-        if(gamepad2.dpad_down){
-            tiltl.setPosition(-0.1);
-            tiltr.setPosition(0.1);
-
-        }
+            }
+            if (gamepad2.y) {
+                    lander.setPower(LanderPower);
+            }
+            if(gamepad2.a){
+                lander.setPower(-LanderPower);
+            }
+            if(gamepad2.left_bumper){
+                tilt1.setPosition(-0.1);
+                tilt2.setPosition(-0.1);
+            }
+            if(gamepad2.right_bumper){
+                tilt1.setPosition(0.1);
+                tilt2.setPosition(0.1);
+            }
         }
 
 
